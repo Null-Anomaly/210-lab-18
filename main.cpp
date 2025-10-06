926f7c1faae92c4ec13f8a0067fc0de178b130af
Null-Anomaly <lbryant019@insite.4cd.edu>
2025-10-05 21:07:09 -0700

    Apprx 50 minute mark.

--- main.cpp
+++ main.cpp
@@ -1,30 +1,43 @@
 #include <iostream>
 using namespace std;
 
+//Struct for the linked list node
 struct Node {
     float value;
     string review;
     Node *next;
 };
 
+//Function prototypes
 void addToHead(Node *, float, string);
 void addToTail(Node *, float, string);
     
-
+//Main function
 int main() 
 {
+    //Variables
     int hot;
     double rating;
     string comment;
     Node *head = nullptr;
 
+    //User input
     cout << "Which linked list method should we use?\n";
     cout << "[1] New nodes are added to the head of each linked list\n";
     cout << "[2] New nodes are added to the tail of each linked list\n";
     cin >> hot;
+
+    //Checks to see if Head or Tail was selected
+    while(hot != 1 && hot != 2)
+    {
+        cout << "Invalid input. Please enter 1 or 2.\n";
+        cin >> hot;
+    }
+
+    //For nodes being added to the head of the list
     if (hot == 1)
     {
-        char adder;
+        char adder = 'y';
         while(adder == 'y')
         {
             cout << "Enter review rating 0-5: \n";
@@ -39,9 +52,11 @@ int main()
 
 
     }
-    else if (hot == 2)
+
+    //For nodes being added to the tail of the list
+    if (hot == 2)
     {
-        char adder;
+        char adder = 'y';
         while(adder == 'y')
         {
             cout << "Enter review rating 0-5: \n";
@@ -55,10 +70,19 @@ int main()
         }
         
     }
-    else
+
+    //Prints out the linked list
+    cout << "Outputting all reviews:\n";
+    int i = 1;
+    double average;
+    for(Node *temp = head; temp != nullptr; temp = temp->next)
     {
-        cout << "Invalid input. Please enter 1 or 2." << endl;
+        cout << "   > Review #" << i << ": " <<  temp->value << ": " << temp->review << "\n";
+        average += temp->value;
+        i++;
     }
+    average = average / (i - 1);
+    cout << "   >Average: " << average << "\n";
 
     return 0;
 }
