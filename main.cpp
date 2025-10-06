6abd78c0562d146e541598e77ae657e744e62e23
Null-Anomaly <lbryant019@insite.4cd.edu>
2025-10-05 20:39:10 -0700

    20 minutes

--- main.cpp
+++ main.cpp
@@ -3,26 +3,39 @@ using namespace std;
 
 struct Node {
     float value;
+    string review;
     Node *next;
 };
 
-
+void addToHead(Node *&, float, string); 
+    
 
 int main() 
 {
     int hot;
     double rating;
     string comment;
+    Node *head = nullptr;
+
     cout << "Which linked list method should we use?\n";
     cout << "[1] New nodes are added to the head of each linked list\n";
     cout << "[2] New nodes are added to the tail of each linked list\n";
     cin >> hot;
     if (hot == 1)
     {
-        cout << "Enter review rating 0-5: \n";
-        cin >> rating;
-        cout << "Enter review comment: \n";
-        cin >> comment;
+        char adder;
+        while(adder == 'y')
+        {
+            cout << "Enter review rating 0-5: \n";
+            cin >> rating;
+            cout << "Enter review comment: \n";
+            cin >> comment;
+            cout << "Enter another review? Y/N: \n";
+            cin >> adder;
+            adder = tolower(adder);
+        }
+
+
     }
     else if (hot == 2)
     {
@@ -34,4 +47,13 @@ int main()
     }
 
     return 0;
+}
+
+void addToHead(Node *&head, float val, string com) 
+{
+    Node *newNode = new Node();
+    newNode->value = val;
+    newNode->review = com;
+    newNode->next = head;
+    head = newNode;
 }
